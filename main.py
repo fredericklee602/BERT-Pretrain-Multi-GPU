7b4ffe9c926ad3a717c1fc7aee2f7d6310cb6845
fredericklee602 <37622644+fredericklee602@users.noreply.github.com>
2023-08-16 11:14:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 
 import os
-import time
+import datetime
 import numpy as np
 import torch
 import logging
@@ -9,25 +9,28 @@ from Config import Config
 from DataManager import DataManager
 from Trainer import Trainer
 from Predictor import Predictor
-
+import sys
 
 
 if __name__ == '__main__':
 
-
     config = Config()
-    os.environ["CUDA_VISIBLE_DEVICES"] = config.cuda_visible_devices
+    config.mode = sys.argv[1]
+    start_time = datetime.datetime.now()
+    if config.mode in ['train', 'test']:
+        print ("Currently using [%s] mode" % (sys.argv[1]))
+            
+        os.environ["CUDA_VISIBLE_DEVICES"] = config.cuda_visible_devices
 
-    # Set Seed，保證结果每次結果一致
-    np.random.seed(1)
-    torch.manual_seed(1)
-    torch.cuda.manual_seed_all(1)
-    torch.backends.cudnn.deterministic = True
-    start_time = time.time()
+        # Set Seed，保證结果每次結果一致
+        np.random.seed(1)
+        torch.manual_seed(1)
+        torch.cuda.manual_seed_all(1)
+        torch.backends.cudnn.deterministic = True
 
-    # 數據處理
-    print('read data...')
-    dm = DataManager(config)
+        # 數據處理
+        print('read data...')
+        dm = DataManager(config)
 
     # 模式
     if config.mode == 'train':
@@ -41,12 +44,19 @@ if __name__ == '__main__':
         # 訓練
         trainer = Trainer(config)
         trainer.train(train_loader, valid_loader, local_rank)
+        torch.distributed.barrier()
+        if local_rank==0:
+            end_time = datetime.datetime.now()
+            print("The train mode time spent:", end_time - start_time)
     elif config.mode == 'test':
         # 測試
         test_loader = dm.get_dataset(mode='test', sampler=False)
         predictor = Predictor(config)
         predictor.predict(test_loader)
+        end_time = datetime.datetime.now()
+        print("The test mode time spent:", end_time - start_time)
     else:
         print("no task going on!")
-        print("you can use one of the following lists to replace the valible of Config.py. ['train', 'test', 'valid'] !")
+        print("Train mode: python -m torch.distributed.launch --nproc_per_node=gpu_num --master_port=port --use_env main.py train")
+        print("Test mode: python main.py test")
         
