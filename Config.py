eacd3c5c9173029cf230939ea261a105fdcf5904
fredericklee602 <37622644+fredericklee602@users.noreply.github.com>
2023-08-16 11:13:40 +0800

    Update Config.py

--- Config.py
+++ Config.py
@@ -9,18 +9,18 @@ class Config(object):
     
     def __init__(self):
         
-        self.mode = 'test'
+        self.mode = ''
         # GPU配置
         self.cuda_visible_devices = '0,1,2,3'                           # 可用的GPU
         self.device = 'cuda'                                      # master GPU
-        self.port = str(random.randint(10000,60000))                # 多卡訓練Process間通信Port
-        self.init_method = 'tcp://localhost:' + self.port           # 多卡訓練的通訊地址
-        self.world_size = 1                                        # Process 數，默認為1
+        # self.port = str(random.randint(10000,60000))                # 多卡訓練Process間通信Port
+        # self.init_method = 'tcp://localhost:' + self.port           # 多卡訓練的通訊地址
+        # self.world_size = 1                                        # Process 數，默認為1
         
         # 訓練配置
         self.whole_words_mask = True                                # 使用是否whole words masking機制
-        self.num_epochs = 10                                       # 迭代次數
-        self.batch_size = 16                                       # 每个批次的大小
+        self.num_epochs = 1                                       # 迭代次數
+        self.batch_size = 16                                       # 每個批次的大小
         self.learning_rate = 3e-5                                   # 學習率
         self.num_warmup_steps = 0.1                                 # warm up步數
         self.sen_max_length = 512                                   # 句子最長長度
